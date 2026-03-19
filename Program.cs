224c239abc551e7f65c95c85765e1df5b81be87f
Hubert Jablonski <s33210@pjwstk.edu.pl>
2026-03-19 20:26:29 +0100

    Rozpatrzenie przypadku nieprawidlowego inputa

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿Console.WriteLine("Witaj w programie statystycznym!");
 Console.WriteLine("Podaj liczby po przecinku:");
 string input = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(input))
+{
+    Console.WriteLine("Błąd: Nie podano żadnych danych!");
+    return;
+}
