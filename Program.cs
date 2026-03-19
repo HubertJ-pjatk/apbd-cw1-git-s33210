cc34254783fe3e9d29b47191800890e1d8b78c33
Hubert Jablonski <s33210@pjwstk.edu.pl>
2026-03-19 20:25:35 +0100

    Wstawienie inputa dla uzytkownika

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Witaj w programie statystycznym!");
+Console.WriteLine("Podaj liczby po przecinku:");
+string input = Console.ReadLine();
